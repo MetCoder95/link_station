6a2225efaebc7a6a8de1b61d56aa0cff4ce42552
Carlos Fuentes <me@metcoder.dev>
2021-11-10 00:08:56 +0100

    fix: address lint issues

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const { Command, flags } = require('@oclif/command')
+const { Command, flags } = require('@oclif/command');
 
-const getBestLinkStation = require('./lib/link-station')
-const { getRandomArrayValue } = require('./lib/utils')
+const getBestLinkStation = require('./lib/link-station');
+const { getRandomArrayValue } = require('./lib/utils');
 
 const DEFAULT_DEVICES = [
   [0, 0],
   [100, 100],
   [15, 10],
-  [18, 18]
-]
+  [18, 18],
+];
 const DEFAULT_LINK_STATIONS = [
   [0, 0, 10],
   [20, 20, 5],
-  [10, 0, 12]
-]
+  [10, 0, 12],
+];
 
 // TODO: Allow multiple devices
 // TODO: read from a file
@@ -23,11 +23,11 @@ const DEFAULT_LINK_STATIONS = [
  * CLI instance
  */
 class CLI extends Command {
-  static description = 'Find the best link station for a given device'
+  static description = 'Find the best link station for a given device';
 
-  static usage = 'link-station [device] '
+  static usage = 'link-station [device] ';
 
-  static examples = ['link-station 1,2 -s 1,2,3 4,5,6']
+  static examples = ['link-station 1,2 -s 1,2,3 4,5,6'];
 
   static args = [
     {
@@ -35,9 +35,9 @@ class CLI extends Command {
       required: false,
       description:
         'The device coordinates to find the best link station for. Are coordinates separated via coma. e.g. X,Y',
-      parse: input => input.split(',').map(Number)
-    }
-  ]
+      parse: (input) => input.split(',').map(Number),
+    },
+  ];
 
   static flags = {
     stations: flags.string({
@@ -46,37 +46,37 @@ class CLI extends Command {
       description:
         'The list of stations to use. Is constituted of three values separated by a comma, Y X R(Reach).',
       multiple: true,
-      parse: input => input.split(',').map(Number)
-    })
-  }
+      parse: (input) => input.split(',').map(Number),
+    }),
+  };
 
-  validateDevice (device) {
+  validateDevice(device) {
     if (!device || device.length === 0) {
       this.error('Device coordinates must be two values separated by a coma', {
-        exit: 1
-      })
+        exit: 1,
+      });
     }
 
-    device.forEach(coordinate => {
+    device.forEach((coordinate) => {
       if (isNaN(parseInt(coordinate))) {
-        this.error('Device coordinates must be a valid number', { exit: 1 })
+        this.error('Device coordinates must be a valid number', { exit: 1 });
       }
-    })
+    });
   }
 
-  validateStations (stations) {
+  validateStations(stations) {
     if (!stations || stations.length < 2) {
-      return this.error('Stations must be at least two values', { exit: 1 })
+      return this.error('Stations must be at least two values', { exit: 1 });
     }
 
     for (const [x, y, reach] of stations) {
       if (isNaN(parseInt(x)) || isNaN(parseInt(y)) || isNaN(parseInt(reach))) {
-        this.error('Stations coordinates must a valid number', { exit: 1 })
+        this.error('Stations coordinates must a valid number', { exit: 1 });
       }
     }
   }
 
-  async run () {
+  async run() {
     const { args, flags } = this.parse(CLI);
     const { device = getRandomArrayValue(DEFAULT_DEVICES) } = args;
     const { stations = DEFAULT_LINK_STATIONS } = flags;
@@ -84,11 +84,15 @@ class CLI extends Command {
     this.validateDevice(device);
     this.validateStations(stations);
 
-    this.log(`Finding the best link station for device ${device}, with Link Stations ${stations.join('/')}`);
+    this.log(
+      `Finding the best link station for device ${device}, with Link Stations ${stations.join(
+        '/'
+      )}`
+    );
     const result = getBestLinkStation(device, stations);
 
-    this.log(result)
+    this.log(result);
   }
 }
 
-module.exports = CLI
+module.exports = CLI;
